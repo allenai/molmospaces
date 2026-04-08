4023d5a39ce2232fd4f6b678c5dd536f84e5e056
ryanlindeborg <lindeborgryan@gmail.com>
2026-04-08 17:25:28 -0400

    Fix camera parameters issue. Set JsonBenchmarkEvalConfig to terminate on success as default

--- molmo_spaces/env/sensors.py
+++ molmo_spaces/env/sensors.py
@@ -1134,7 +1134,9 @@ def get_core_sensors(exp_config):
 
         # Camera parameter sensor
         cam_params = CameraParameterSensor(
-            camera_name=camera_name, uuid=f"sensor_param_{camera_name}"
+            camera_name=camera_name,
+            uuid=f"sensor_param_{camera_name}",
+            img_resolution=exp_config.camera_config.img_resolution,
         )
         sensors.append(cam_params)
 
@@ -1248,7 +1250,9 @@ def get_nav_task_sensors(exp_config):
 
         # Camera parameter sensor
         cam_params = CameraParameterSensor(
-            camera_name=camera_name, uuid=f"sensor_param_{camera_name}"
+            camera_name=camera_name,
+            uuid=f"sensor_param_{camera_name}",
+            img_resolution=exp_config.camera_config.img_resolution,
         )
         sensors.append(cam_params)
 
