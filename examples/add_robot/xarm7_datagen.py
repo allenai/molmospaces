89fc4f09686c48bc11930dc197b8da280cf16f9d
Max Argus <maxa@allenai.org>
2026-08-01 00:29:19 -0700

    default image resolution from 352x624 to 360x640

--- examples/add_robot/xarm7_datagen.py
+++ examples/add_robot/xarm7_datagen.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 
+from xarm7_config import XArm7RobotConfig
+
 from molmo_spaces.configs import PickTaskSamplerConfig
 from molmo_spaces.configs.base_pick_config import PickBaseConfig
 from molmo_spaces.configs.camera_configs import AllCameraTypes, CameraSystemConfig, MjcfCameraConfig
 from molmo_spaces.data_generation.config_registry import register_config
-
 from molmo_spaces.tasks.pick_task_sampler import PickTaskSampler
-from xarm7_config import XArm7RobotConfig
 
 
 class XArm7CameraSystem(CameraSystemConfig):
-    img_resolution: tuple[int, int] = (624, 352)
+    img_resolution: tuple[int, int] = (640, 360)
 
     cameras: list[AllCameraTypes] = [
         MjcfCameraConfig(
